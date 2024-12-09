179c86d2670a8160f74a9b9b9b77508b526a9d2a
exdysa <91800957+exdysa@users.noreply.github.com>
2024-12-09 01:07:34 -0500

    adjust file structure

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-from setuptools import setup, find_packages
-import os
-import sys
-setup(
-    name='nnll',
-    version='0.0.0',
-    packages=find_packages(),
-)
---
+++ test_import.py
@@ -0,0 +1,8 @@
+
+from nnll_08.src import soft_random
+from nnll_08.src import hard_random
+
+print(soft_random.name)
+print(soft_random.__path__)
+print(hard_random.name)
+print(hard_random.__path__)
