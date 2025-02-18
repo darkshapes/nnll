03c76ecbe00a708e05631580f4c7d634a565a502
exdysa <91800957+exdysa@users.noreply.github.com>
2025-02-18 10:50:32 -0500

    patch safetensors routine

--- modules/nnll_04/test_src.py
+++ modules/nnll_04/test_src.py
@@ -11,17 +11,18 @@ from modules.nnll_45.src import download_hub_file
 
 class TestLoadMetadataSafetensors(unittest.TestCase):
     def test_metadata_from_safetensors(self):
-        self.local_folder = os.path.dirname(os.path.abspath(__file__))
-        self.local_folder_test = os.path.join(self.local_folder, "test_folder")
-        self.folder_path_named, folder_contents = download_hub_file(repo_id="exdysa/tiny-random-gpt2-bfloat16", filename="model.safetensors", local_dir=self.local_folder_test)
-        real_file = os.path.join(self.folder_path_named, next(iter(folder_contents)))
+        local_folder = os.path.dirname(os.path.abspath(__file__))
+        local_folder_test = os.path.join(local_folder, "test_folder")
+        file_name = "model.safetensors"
+        folder_path_named, folder_contents = download_hub_file(repo_id="exdysa/tiny-random-gpt2-bfloat16", filename=file_name, local_dir=local_folder_test)
+        real_file = os.path.join(folder_path_named, file_name)
         virtual_data_00 = metadata_from_safetensors(real_file)
-        safetensors_state_dict = os.path.join(self.local_folder, "expected_output.json")
+        safetensors_state_dict = os.path.join(local_folder, "expected_output.json")
         expected_output = read_json_file(safetensors_state_dict)
         assert virtual_data_00 == expected_output
         try:
-            shutil.rmtree(self.local_folder_test)
-            shutil.rmtree(os.path.join(self.local_folder, ".cache"))
+            shutil.rmtree(local_folder_test)
+            shutil.rmtree(os.path.join(local_folder, ".cache"))
         except OSError:
             pass
 
