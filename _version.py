d80383989158236ba577445d99a2484a5cbe1376
exdysa <91800957+exdysa@users.noreply.github.com>
2025-02-27 11:50:56 -0500

    Patch pyproject for removed modules

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev141+gc3ca03a.d20250227'
-__version_tuple__ = version_tuple = (0, 1, 'dev141', 'gc3ca03a.d20250227')
+__version__ = version = '0.1.dev143+g77daa09.d20250227'
+__version_tuple__ = version_tuple = (0, 1, 'dev143', 'g77daa09.d20250227')
