0ec56349f16b76050c6337b8c13ca38b8fa97b63
exdysa <91800957+exdysa@users.noreply.github.com>
2025-03-20 20:10:12 -0400

    Version

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev173+g8860202.d20250320'
-__version_tuple__ = version_tuple = (0, 1, 'dev173', 'g8860202.d20250320')
+__version__ = version = '0.1.dev174+gb01b37d.d20250321'
+__version_tuple__ = version_tuple = (0, 1, 'dev174', 'gb01b37d.d20250321')
