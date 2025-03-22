68793abd0a086a1b3356ea738d6888432103d9bc
exdysa <91800957+exdysa@users.noreply.github.com>
2025-03-21 22:21:57 -0400

    libportaudio2

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev200+g77bfb68.d20250322'
-__version_tuple__ = version_tuple = (0, 1, 'dev200', 'g77bfb68.d20250322')
+__version__ = version = '0.1.dev201+g341e3cf.d20250322'
+__version_tuple__ = version_tuple = (0, 1, 'dev201', 'g341e3cf.d20250322')
