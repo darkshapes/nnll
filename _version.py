ca0535f11ffc85cb710eba408228c6dfd7827579
exdysa <91800957+exdysa@users.noreply.github.com>
2025-03-31 21:47:27 -0400

    ~dependency regression

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev227+g2fcadee.d20250401'
-__version_tuple__ = version_tuple = (0, 1, 'dev227', 'g2fcadee.d20250401')
+__version__ = version = '0.1.dev228+g522c0f0.d20250401'
+__version_tuple__ = version_tuple = (0, 1, 'dev228', 'g522c0f0.d20250401')
