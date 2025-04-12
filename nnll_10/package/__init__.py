2edd6b531926741f3a8c700008c3c8ba758357bd
exdysa <91800957+exdysa@users.noreply.github.com>
2025-04-12 15:20:37 -0400

    ~Re-update cli commands

--- nnll_10/package/__init__.py
+++ nnll_10/package/__init__.py
@@ -5,28 +5,29 @@ import os
 sys.path.append(os.getcwd())
 
 
-"""Launch textual UI"""
-from nnll_10.package.__main__ import Combo
-from nnll_01 import info_message as nfo
+def main():
+    """Launch textual UI"""
+    from nnll_10.package.__main__ import Combo
+    from nnll_01 import info_message as nfo
 
-trace = False
-if sys.argv[0] == "-t" or sys.argv[0] == "--trace":
-    import litellm
-    from viztracer import VizTracer
+    trace = False
+    if sys.argv[0] == "-t" or sys.argv[0] == "--trace":
+        import litellm
+        from viztracer import VizTracer
 
-    litellm.suppress_debug_info = False
-    trace = True
-    tracer = VizTracer()
-    tracer.start()
+        litellm.suppress_debug_info = False
+        trace = True
+        tracer = VizTracer()
+        tracer.start()
 
-app = Combo(ansi_color=False)
+    app = Combo(ansi_color=False)
 
-nfo("Launching...")
-app.run()
-if trace:
-    from datetime import datetime
+    nfo("Launching...")
+    app.run()
+    if trace:
+        from datetime import datetime
 
-    os.makedirs("log", exist_ok=True)
-    assembled_path = os.path.join("log", f".nnll{datetime.now().strftime('%Y%m%d')}_trace.json")
-    tracer.stop()
-    tracer.save(output_file=assembled_path)  # also takes output_file as an optional argument
+        os.makedirs("log", exist_ok=True)
+        assembled_path = os.path.join("log", f".nnll{datetime.now().strftime('%Y%m%d')}_trace.json")
+        tracer.stop()
+        tracer.save(output_file=assembled_path)  # also takes output_file as an optional argument
