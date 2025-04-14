c254a09b4a6b68c88de0526878e1d4957faa36a5
exdysa <91800957+exdysa@users.noreply.github.com>
2025-04-14 01:03:24 -0400

    +Rebuild model field, +small bufgixes

--- nnll_10/package/carousel.py
+++ nnll_10/package/carousel.py
@@ -7,47 +7,59 @@ from textual.widgets import DataTable
 from textual.screen import Screen
 from textual.reactive import reactive
 
-from nnll_01 import debug_monitor
+from nnll_01 import debug_monitor, info_message as nfo  # debug_message as dbug,
 
 
 class Carousel(DataTable):
     """Revolving text-line component based on default DataTable widget class"""
 
     nx_graph: dict
-    content_cell: reactive[int] = reactive(0.0)
+    content_cell: reactive[int] = reactive(0)
 
     up = "[@click='scroll_button(1)']▲[/]"
     dwn = "[@click='scroll_button']▼[/]"
+    current_cell = "text"
+    current_row = 0
+    y_coord = 1
+    scroll_counter = 5
 
     def on_mount(self) -> None:
         self.show_header = False
         self.cursor_type = "cell"
 
     @debug_monitor
-    def emulate_scroll_down(self, interval: float = 0.05) -> str:
+    def emulate_scroll_down(self, interval: int = 1) -> str:
         """Trigger datatable cursor movement using fractional sensitivity
         :param ceiling: Total entry count of the table *column*
         :return: The datata in the table *row*
         """
-        if self.content_cell < len(self.columns) - 1:
-            self.content_cell += interval
-        coordinate = abs(int(round(self.content_cell)))
-        self.move_cursor(row=coordinate, column=1)
-        current_content = self.get_cell_at((coordinate, 1))
-        return current_content
+
+        if (self.row_count - 1) >= (self.current_row + interval):
+            if self.y_coord < self.scroll_counter:
+                self.y_coord += interval
+            else:
+                self.y_coord = interval
+                self.current_row += interval
+                self.move_cursor(row=self.current_row, column=1)
+        self.current_cell = self.get_cell_at((self.current_row, 1))
+        return self.current_cell
 
     @debug_monitor
-    def emulate_scroll_up(self, interval: float = 0.05) -> str:
+    def emulate_scroll_up(self, interval: int = 1) -> str:
         """Trigger datatable cursor movement using fractional sensitivity
         :param ceiling: Total entry count of the table *column*
         :return: The datata in the table *row*
         """
-        if self.content_cell > 0:
-            self.content_cell -= interval
-        coordinate = abs(int(round(self.content_cell)))
-        self.move_cursor(row=coordinate, column=1)
-        current_content = self.get_cell_at((coordinate, 1))
-        return current_content
+
+        if 0 <= self.current_row - interval:
+            if self.y_coord >= 1:
+                self.y_coord -= interval
+            else:
+                self.y_coord = self.scroll_counter
+                self.current_row -= interval
+                self.move_cursor(row=self.current_row, column=1)
+        self.current_cell = self.get_cell_at((self.current_row, 1))
+        return self.current_cell
 
     @debug_monitor
     def action_scroll_button(self, up: bool = False) -> None:
@@ -55,10 +67,12 @@ class Carousel(DataTable):
         :param up: Scroll direction, defaults to False
         """
         if up:
-            content = self.emulate_scroll_up(interval=0.6)
+            self.current_cell = self.emulate_scroll_up(interval=self.y_coord + 1)
             if self.id == "input_tag":
-                self.query_ancestor(Screen).foldr["ps"].current = self.query_ancestor(Screen).input_map[content]
+                self.query_ancestor(Screen).foldr["ps"].current = self.query_ancestor(Screen).input_map[self.current_cell]
+                self.query_ancestor(Screen).ready_tx(io_only=True)
         else:
-            content = self.emulate_scroll_down(interval=0.6)
+            self.current_cell = self.emulate_scroll_down(interval=self.scroll_counter - self.y_coord - 1)
             if self.id == "input_tag":
-                self.query_ancestor(Screen).foldr["ps"].current = self.query_ancestor(Screen).input_map[content]
+                self.query_ancestor(Screen).foldr["ps"].current = self.query_ancestor(Screen).input_map[self.current_cell]
+                self.query_ancestor(Screen).ready_tx(io_only=True)
