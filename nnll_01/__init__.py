a3a3ec9db671ed104e7d9af847024e8912ee0490
exdysa <91800957+exdysa@users.noreply.github.com>
2025-04-26 16:59:47 -0400

    ~disable litellm log in 01

--- nnll_01/__init__.py
+++ nnll_01/__init__.py
@@ -56,7 +56,8 @@ def configure_logging(file_name: str = ".nnll", folder_path_named: str = "log", 
 
     import logging
     import structlog
-    import litellm
+
+    # import litellm
     from structlog.processors import ExceptionPrettyPrinter, StackInfoRenderer, format_exc_info, dict_tracebacks, TimeStamper, JSONRenderer
     from structlog.stdlib import add_log_level, PositionalArgumentsFormatter
     from structlog import configure as structlog_conf, make_filtering_bound_logger, WriteLoggerFactory, get_logger
@@ -97,9 +98,9 @@ def configure_logging(file_name: str = ".nnll", folder_path_named: str = "log", 
             ),
         ],
     )
-    litellm.disable_streaming_logging = True
-    litellm.turn_off_message_logging = True
-    litellm.suppress_debug_info = True
+    # litellm.disable_streaming_logging = True
+    # litellm.turn_off_message_logging = True
+    # litellm.suppress_debug_info = True
     # litellm.json_logs = True  # type: ignore
     handler = logging.FileHandler(assembled_path)
     handler.setFormatter(formatter)
