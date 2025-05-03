4f37e39eae0e99fc58430467d08911cc191db25c
exdysa <91800957+exdysa@users.noreply.github.com>
2025-05-03 13:56:52 -0400

    ~patch available tasks processing

--- nnll_15/__init__.py
+++ nnll_15/__init__.py
@@ -37,14 +37,14 @@ class RegistryEntry(BaseModel):
         processed_tasks = []
         library_tasks = VALID_TASKS[self.library]
         if self.library in [LibType.OLLAMA, LibType.LM_STUDIO, LibType.LLAMAFILE, LibType.CORTEX, LibType.VLLM]:
-            default_task = ("text", "text") # usually these are txt gen libraries
-        elif self.library == LibType.HUB: # pair tags from the hub such 'x-to-y' such as 'text-to-text' etc
+            default_task = ("text", "text")  # usually these are txt gen libraries
+        elif self.library == LibType.HUB:  # pair tags from the hub such 'x-to-y' such as 'text-to-text' etc
             pattern = re.compile(r"(\w+)-to-(\w+)")
             for tag in self.tags:
                 match = pattern.search(tag)
-                if match and all(group in VALID_CONVERSIONS for group in match.groups()):
+                if match and all(group in VALID_CONVERSIONS for group in match.groups()) and (match.group(1), match.group(2)) not in processed_tasks:
                     processed_tasks.append((match.group(1), match.group(2)))
-        for tag in self.tags: # when pair-tagged elements are not available, potential to duplicate HUB tags here
+        for tag in self.tags:  # when pair-tagged elements are not available, potential to duplicate HUB tags here
             for (graph_src, graph_dest), tags in library_tasks.items():
                 if tag in tags and (graph_src, graph_dest) not in processed_tasks:
                     processed_tasks.append((graph_src, graph_dest))
@@ -61,12 +61,12 @@ class RegistryEntry(BaseModel):
         entries = []
 
         @LIBTYPE_CONFIG.decorator
-        def _read_data(data:dict =None):
+        def _read_data(data: dict = None):
             return data
 
         api_data = _read_data()
 
-        if next(iter(LibType.OLLAMA.value)) and has_api("OLLAMA"): # check that server is still up!
+        if next(iter(LibType.OLLAMA.value)) and has_api("OLLAMA"):  # check that server is still up!
             from ollama import ListResponse, list as ollama_list
 
             model_data: ListResponse = ollama_list()  # type: ignore
@@ -135,7 +135,7 @@ class RegistryEntry(BaseModel):
             model_data = OpenAI(base_url=api_data["VLLM"]["api_kwargs"]["api_base"], api_key=api_data["VLLM"]["api_kwargs"]["api_key"])
             for model in model_data.models.list().data:
                 entry = cls(
-                    model = f"{api_data[LibType.VLLM.value[1]].get('prefix')}{model['data'].get('id')}f",
+                    model=f"{api_data[LibType.VLLM.value[1]].get('prefix')}{model['data'].get('id')}f",
                     size=0,
                     tags=["text"],
                     library=LibType.VLLM,
@@ -145,6 +145,7 @@ class RegistryEntry(BaseModel):
 
         if next(iter(LibType.LM_STUDIO.value)) and has_api("LM_STUDIO"):
             from lmstudio import list_downloaded_models  # pylint: disable=import-error, # type: ignore
+
             model_data = list_downloaded_models()
             for model in model_data:  # pylint:disable=no-member
                 tags = []
