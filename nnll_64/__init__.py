ab2d59c13cc8a68e5e1bcdc694f0c464d6d1b919
exdysa <91800957+exdysa@users.noreply.github.com>
2025-05-17 14:38:29 -0400

    test~

--- nnll_64/__init__.py
+++ nnll_64/__init__.py
@@ -47,8 +47,10 @@ def run_inference(mir_arch: str, lora_opt: list = None) -> None:
     # data_chain = HyperChain()
     factory = ConstructPipeline()
     pipe_name, pipe_mode, pipe_kwargs, model, kwargs = factory.create_pipeline(architecture=mir_arch)
-    pipe_class = getattr(pipe_name, pipe_mode)
-    pipe = pipe_class(model, **pipe_kwargs).to(active_gpu)
+    # pipe_class = getattr(pipe_name, pipe_mode)
+    from diffusers import CogView3PlusPipeline
+
+    pipe = CogView3PlusPipeline.from_pretrainined(model, **pipe_kwargs).to(active_gpu)
 
     nfo(f"pre-generator Model {model} Lora {lora} Arguments {kwargs} {pipe}")
     if lora:
