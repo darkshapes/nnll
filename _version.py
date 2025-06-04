b0e490fbf01d12bab44fbf8bfa385dcb2a54dfd2
exdysa <91800957+exdysa@users.noreply.github.com>
2025-06-04 19:23:22 -0400

    3.13

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev533+gc8da14c.d20250604'
-__version_tuple__ = version_tuple = (0, 1, 'dev533', 'gc8da14c.d20250604')
+__version__ = version = '0.1.dev534+gd21396f.d20250604'
+__version_tuple__ = version_tuple = (0, 1, 'dev534', 'gd21396f.d20250604')
