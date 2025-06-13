9a8b3e9e7d6aee33fe9ea17471a9108648ab0dda
exdysa <91800957+exdysa@users.noreply.github.com>
2025-06-12 21:22:50 -0400

    ~Adjust deps (Again)

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev570+g641c3a4.d20250611'
-__version_tuple__ = version_tuple = (0, 1, 'dev570', 'g641c3a4.d20250611')
+__version__ = version = '0.1.dev573+g5b756bb.d20250613'
+__version_tuple__ = version_tuple = (0, 1, 'dev573', 'g5b756bb.d20250613')
