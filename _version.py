9f30b185fd36eb5dc5ad24fa8b78ec932b049133
exdysa <91800957+exdysa@users.noreply.github.com>
2025-06-14 19:16:57 -0400

    ~Patch hub d/l

--- _version.py
+++ _version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev584+gcf72888.d20250614'
-__version_tuple__ = version_tuple = (0, 1, 'dev584', 'gcf72888.d20250614')
+__version__ = version = '0.1.dev585+g8f3272d.d20250614'
+__version_tuple__ = version_tuple = (0, 1, 'dev585', 'g8f3272d.d20250614')
